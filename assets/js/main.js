4917f18c8022afd6060dabf22d6f14c00fa73a5d
damydavo <damydavo2015@gmail.com>
2020-09-11 16:43:35 -0700

    modified the content

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-tablinks = document.getElementsByClassName("tablinks");
-for (i = 0; i < tablinks.length; i++) {
-  tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
-document.getElementById(cityName).style.display = "block";
-evt.currentTarget.className += " active";
